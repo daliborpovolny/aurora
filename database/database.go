609a93c10c4cb665bd6c10afad1a7fde877e9027
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-07-03 22:24:02 +0200

    fix Seed method

--- database/database.go
+++ database/database.go
@@ -22,7 +22,6 @@ func Initialize() *sql.DB {
 
 	if resetDB == "true" {
 		os.Remove("./database/db_file.db")
-		Seed()
 	}
 
 	ctx := context.Background()
@@ -40,51 +39,55 @@ func Initialize() *sql.DB {
 
 	Queries = database.New(db)
 
+	if resetDB == "true" {
+		Seed(Queries)
+	}
+
 	return db
 }
 
-func Seed() {
+func Seed(q *database.Queries) {
 	ctx := context.Background()
-	if users, _ := Queries.ListUsers(ctx); len(users) == 0 {
-		Queries.CreateUser(ctx, database.CreateUserParams{
+	if users, _ := q.ListUsers(ctx); len(users) == 0 {
+		q.CreateUser(ctx, database.CreateUserParams{
 			FirstName: "par",
 			LastName:  "ent",
 			Email:     "par.ent@gmail.com",
 			Hash:      "parentHash",
 		})
 
-		Queries.CreateUser(ctx, database.CreateUserParams{
+		q.CreateUser(ctx, database.CreateUserParams{
 			FirstName: "tea",
 			LastName:  "cher",
 			Email:     "tea.cher@isic.com",
 			Hash:      "teachingHash",
 		})
 
-		Queries.CreateUser(ctx, database.CreateUserParams{
+		q.CreateUser(ctx, database.CreateUserParams{
 			FirstName: "stu",
 			LastName:  "end",
 			Email:     "stu.dent@school.com",
 			Hash:      "studentsHash",
 		})
 
-		Queries.CreateUser(ctx, database.CreateUserParams{
+		q.CreateUser(ctx, database.CreateUserParams{
 			FirstName: "ad",
 			LastName:  "min",
 			Email:     "ad.min@manage.com",
 			Hash:      "adminsHash",
 		})
 
-		_, err := Queries.CreateParent(ctx, 1)
+		_, err := q.CreateParent(ctx, 1)
 		if err != nil {
 			panic(err)
 		}
 
-		teacher, err := Queries.CreateTeacher(ctx, 2)
+		teacher, err := q.CreateTeacher(ctx, 2)
 		if err != nil {
 			panic(err)
 		}
 
-		class, err := Queries.CreateClass(ctx, database.CreateClassParams{
+		class, err := q.CreateClass(ctx, database.CreateClassParams{
 			TeacherID:      teacher.ID,
 			Room:           "A24",
 			StartYear:      2024,
@@ -94,14 +97,14 @@ func Seed() {
 			panic(err)
 		}
 
-		Queries.CreateStudent(ctx, database.CreateStudentParams{
+		q.CreateStudent(ctx, database.CreateStudentParams{
 			UserID:  3,
 			ClassID: class.ID,
 		})
 
-		Queries.CreateAdmin(ctx, 4)
+		q.CreateAdmin(ctx, 4)
 
-		Queries.AssignStudentToParent(ctx, database.AssignStudentToParentParams{
+		q.AssignStudentToParent(ctx, database.AssignStudentToParentParams{
 			ParentID:  1,
 			StudentID: 1,
 		})
